511a144a8542469c830e6cb047f38eb462dafc73
hunglt92 <hungtrong@Hungs-MacBook-Pro.local>
2013-09-23 07:48:34 +0700

    POLYBOOK
    version 0.1

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  POLYBOOK
+//
+//  Created by .:L.T.H:. on 9/22/13.
+//  Copyright (c) 2013 .:L.T.H:. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  POLYBOOK
+//
+//  Created by .:L.T.H:. on 9/22/13.
+//  Copyright (c) 2013 .:L.T.H:. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
